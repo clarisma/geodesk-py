e4adb367cb18d1c456231c93c2431572dcd2a3fc
clarisma <56932851+clarisma@users.noreply.github.com>
2023-12-10 15:34:43 +0100

    test

--- test/test_filter.py
+++ test/test_filter.py
@@ -1,5 +1,14 @@
 
 def test_filter(features):
-    for street in features("w[highway]").filter(lambda f: len(f.tags) == 3):    
-        print(street)
-        assert len(street.tags) == 3
+    count = 0
+    for street in features("w[highway=primary]").filter(lambda f: len(f.tags) == 3):    
+        # assert len(street.tags) == 3
+        count += 1
+    print(f"{count} primary streets have 3 tags")    
+    
+def test_filter_manual(features):
+    count = 0
+    for street in features("w[highway=primary]"):
+        if len(street.tags) == 3:
+            count += 1
+    print(f"{count} primary streets have 3 tags")    
