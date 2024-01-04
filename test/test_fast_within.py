6e84eabf8c0c168dddb9c7e1578f51337d2bcb19
clarisma <56932851+clarisma@users.noreply.github.com>
2024-01-04 15:06:38 +0100

    tests for within

--- test/test_fast_within.py
+++ test/test_fast_within.py
@@ -3,6 +3,23 @@ def test_performance_intersects(features):
         "a[boundary=administrative]"
         "[admin_level=2][name='United States']").one
     buildings = features("a[building]")
-    
-    print(f"{buildings.within(usa).count} buildings in USA")
-    print(f"{buildings.relations.within(usa).count} relation-buildings in USA")
+
+    with open("c:\\geodesk\\debug\\slow-rels.txt", 'w') as file:
+        for rel in buildings.relations.within(usa):
+            file.write(f"{rel.id}\n")
+
+def read_ids(file_path):
+    ids = set()
+    with open(file_path, 'r') as file:
+        for line in file:
+            id = line.strip()
+            if id.isdigit():
+                ids.add(int(id))
+    return ids
+
+# Function to compare set A and set B (which both contain only integers) and prints the numbers from A that are not in B
+def compare_sets_and_print_difference(set_a, set_b):
+    difference = set_a - set_b
+    for id in difference:
+        print(id)
+
