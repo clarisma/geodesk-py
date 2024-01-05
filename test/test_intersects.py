f1387619436fd30ea9c4ed766ddb998534fb0708
clarisma <56932851+clarisma@users.noreply.github.com>
2024-01-05 12:59:31 +0100

    test for #31

--- test/test_intersects.py
+++ test/test_intersects.py
@@ -1,7 +1,11 @@
 # Copyright (c) 2024 Clarisma / GeoDesk contributors
 # SPDX-License-Identifier: LGPL-3.0-only
 
-def test_intersects(features):
+import geodesk
+import json
+from shapely.geometry import shape
+
+def notest_intersects(features):
     """
     - An `intersects` query should return the same number of features,
      regardless whether we use the area feature or its shape
@@ -19,3 +23,17 @@ def test_intersects(features):
     in_city = features.intersects(city)
     assert streets(in_city).count == count
     assert (streets & in_city).count == count
+    
+def test_intersects_contains_mp(features):
+    """
+    A candidate feature intersects a test geometry that is a multipolygon,
+    even if the candidate only contains one of the test MP's polygons.
+    (Issue #31)
+    """
+    geojson_string = '{"type": "MultiPolygon", "coordinates": [[[[1.130858, 47.665071], [0.0761706, 46.9955999], [1.4824205, 46.2265974], [2.7568345, 46.5445679], [2.9326157, 47.2196999], [2.2294907, 48.0333352], [1.130858, 47.665071]]], [[[11.5492744, 48.1454669], [11.5410347, 48.1408897], [11.553566, 48.1373421], [11.5492744, 48.1454669]]]]}'
+    geojson_dict = json.loads(geojson_string)
+    geom = geodesk.to_mercator(shape(geojson_dict))
+    bavaria = features("a[boundary=administrative][admin_level=4][name:en=Bavaria]")
+    assert geom.intersects(bavaria.one.shape)
+    assert bavaria.one.shape.intersects(geom)
+    assert bavaria.intersects(geom)
--- test/test_main.py
+++ test/test_main.py
@@ -26,6 +26,7 @@ if __name__ == '__main__':
     # pytest.main(["test_match.py", "-rA", "-s", "--durations=9999"])
     # pytest.main(["test_crosses.py", "-rA", "-s", "--durations=9999"])
     # pytest.main(["test_map.py", "-rA", "-s", "--durations=9999"])
-    pytest.main(["test_fast_within.py", "-rA", "-s", "--durations=9999"])
+    # pytest.main(["test_fast_within.py", "-rA", "-s", "--durations=9999"])
+    pytest.main(["test_intersects.py", "-rA", "-s", "--durations=9999"])
 
     
