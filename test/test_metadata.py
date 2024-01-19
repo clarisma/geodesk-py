fb63ad656070a7a3fe4b403860051d547b7718bf
clarisma <56932851+clarisma@users.noreply.github.com>
2024-01-19 16:36:08 +0100

    Tile.size

--- test/test_metadata.py
+++ test/test_metadata.py
@@ -19,3 +19,13 @@ def test_indexed_keys(features):
     assert "highway" in indexed_keys
     # There must not be any duplicates
     assert len(indexed_keys) == len(set(indexed_keys))
+    
+def notest_tile_size(features):
+    tile_count = 0 
+    total_size = 0
+    for tile in features.tiles:
+        size = tile.size
+        print(f"{tile}: {size} bytes")
+        total_size += size
+        tile_count += 1
+    print(f"{tile_count} tiles with {total_size} bytes total")        
