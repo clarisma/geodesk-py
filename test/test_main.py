90b2834eab1bc4ba3800cb26bbdd7e5ed973ebae
clarisma <56932851+clarisma@users.noreply.github.com>
2024-02-23 16:34:42 +0100

    x

--- test/test_main.py
+++ test/test_main.py
@@ -14,7 +14,7 @@ if __name__ == '__main__':
             print(f"- {member} as {member.role}")
     """
     
-    pytest.main(["-rA", "-s", "--durations=9999"])
+    # pytest.main(["-rA", "-s", "--durations=9999"])
     # pytest.main(["test_features.py::test_query_parser", "-rA", "--durations=9999"])
     # pytest.main(["test_rtree.py::test_rtree", "-rA", "--durations=9999"])
     # pytest.main(["test_polygonizer.py::test_polygonizer", "-rA", "-s", "--durations=9999"])
@@ -32,6 +32,6 @@ if __name__ == '__main__':
     # pytest.main(["test_coordinate.py", "-rA", "-s", "--durations=9999"])
     # pytest.main(["test_format.py::test_numeric_custom_ids", "-rA", "-s", "--durations=9999"])
     # pytest.main(["test_anonymous_nodes.py", "-rA", "-s", "--durations=9999"])
-    # pytest.main(["geom/test_shape.py", "-rA", "-s", "--durations=9999"])
+    pytest.main(["test_build.py", "-rA", "-s", "--durations=9999"])
 
     
