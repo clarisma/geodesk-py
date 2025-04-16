4e738fd0ae958a0e9800b4f5ee83a85995a460f0
clarisma <56932851+clarisma@users.noreply.github.com>
2025-04-17 01:54:07 +0200

    Update submodules to latest commits

--- setup.py
+++ setup.py
@@ -7,14 +7,14 @@ this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
 
 setup(
-    name="geodesk",
+    name="geodesk2",
     # version="0.0.6",  # already defined in pyproject.toml
     description='Python port of GeoDesk, a fast and storage-efficient spatial database engine for OpenStreetMap features',
     long_description=long_description,
     long_description_content_type='text/markdown',
     author="Clarisma / GeoDesk contributors",
     license="LGPL-3.0-only",  
-    packages=["geodesk"],  # If you have Python packages as well
+    packages=["geodesk2"],  # If you have Python packages as well
     cmake_args=['-DCMAKE_BUILD_TYPE=Release', '-DBUILD_WHEELS:BOOL=ON'],  # or Debug, or any other args you need
     # install_requires=[  # If you have Python dependencies
     #     "somepythonpackage>=1.0",
