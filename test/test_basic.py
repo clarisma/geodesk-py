4cb436d4d81f669999104dae7408b66cfa36b5b8
clarisma <56932851+clarisma@users.noreply.github.com>
2025-04-17 13:56:41 +0200

    Update submodules to latest commits

--- test/test_basic.py
+++ test/test_basic.py
@@ -1,5 +1,5 @@
-import geodesk2
+from geodesk2 import *
 import test_concur
 
 def test_basic():
-    test_concur.test_concur(geodesk.Features("data/monaco"))
+    test_concur.test_concur(Features("data/monaco"))
